7395136ff26c9381196be4796e7f7f1c7e147726
Julie <julienne.bay@gmail.com>
2020-06-16 14:58:07 -0400

    fetches API containing information about different locations, and creates a table

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [addresses, setAddresses] = useState([]);
+
+  function fetchData() {
+    fetch("https://api.ontario.ca/api/data/64029?count=0&download=1").then(
+      response => {
+        response.json().then(addressArray => {
+          setAddresses(addressArray);
+        });
+      }
+    );
+  }
+
+  if (addresses.length === 0) {
+    fetchData();
+  }
+
+  //search places by any keyword
+  const [searchKeyword, setSearchKeyword] = useState("");
+
+  const updateSearchKeyword = event => {
+    setSearchKeyword(event.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="top-bar">
+        <h2>Ontario hunting and fishing licence issuers: 2020</h2>
+        <h5>
+          Find a nearby location that sells hunting and fishing licences, tags
+          and related products and services.
+        </h5>
+        <div className="search-box">
+          <input
+            id="name-input"
+            type="text"
+            value={searchKeyword}
+            onChange={updateSearchKeyword}
+            placeholder="search by any keyword"
+          />
+        </div>
+      </div>
+      <table>
+        <thead>
+          <tr>
+            <th>City</th>
+            <th>Business Name</th>
+            <th>Address (click to map)</th>
+            <th>Postal Code</th>
+            <th>Issuer Type</th>
+          </tr>
+        </thead>
+        <tbody>
+          {addresses.map((address, i) => {
+            return (
+              <tr key={i}>
+                <td>{address._city.content}</td>
+                <td>{address.business_name.content}</td>
+                <td>{address.street_address.content}</td>
+                <td>{address.postal_code.content}</td>
+                <td>{address.issuer_type.content}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
