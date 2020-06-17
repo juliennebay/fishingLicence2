33f33a22c2d31fcbce657979f5ad1342c0eb2b04
Julie <julienne.bay@gmail.com>
2020-06-17 14:25:52 -0400

    Updates

---
+++ precache-manifest.17f0b1f1d1a50b573f85fcb4ee86ca43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "862919590586d89856b15d5e99f4520e",
+    "url": "/fishingLicence2/index.html"
+  },
+  {
+    "revision": "5ec3ebe10e57d6119e99",
+    "url": "/fishingLicence2/static/css/main.6760e7c6.chunk.css"
+  },
+  {
+    "revision": "2e8feed9898e4ce66466",
+    "url": "/fishingLicence2/static/js/2.03709875.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fishingLicence2/static/js/2.03709875.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ec3ebe10e57d6119e99",
+    "url": "/fishingLicence2/static/js/main.177fb1a9.chunk.js"
+  },
+  {
+    "revision": "a0032322969626bbc133",
+    "url": "/fishingLicence2/static/js/runtime-main.77723717.js"
+  }
+]);
