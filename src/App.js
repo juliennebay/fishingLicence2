577c158bb27f1174131fd4f2d90244affc68c7b5
Julie <julienne.bay@gmail.com>
2020-06-16 21:06:35 -0400

    search function shows locations with matching keywords

--- src/App.js
+++ src/App.js
@@ -25,6 +25,9 @@ function App() {
     setSearchKeyword(event.target.value);
   };
 
+  console.log(addresses[0]);
+  //console.log(Object.values(addresses));
+
   return (
     <div className="App">
       <div className="top-bar">
@@ -54,17 +57,27 @@ function App() {
           </tr>
         </thead>
         <tbody>
-          {addresses.map((address, i) => {
-            return (
-              <tr key={i}>
-                <td>{address._city.content}</td>
-                <td>{address.business_name.content}</td>
-                <td>{address.street_address.content}</td>
-                <td>{address.postal_code.content}</td>
-                <td>{address.issuer_type.content}</td>
-              </tr>
-            );
-          })}
+          {addresses
+            .filter(
+              address =>
+                searchKeyword.length === 0 ||
+                Object.values(address)
+                  .map(obj => obj.content)
+                  .some(content =>
+                    content.toLowerCase().includes(searchKeyword.toLowerCase())
+                  )
+            )
+            .map((address, i) => {
+              return (
+                <tr key={i}>
+                  <td>{address._city.content}</td>
+                  <td>{address.business_name.content}</td>
+                  <td>{address.street_address.content}</td>
+                  <td>{address.postal_code.content}</td>
+                  <td>{address.issuer_type.content}</td>
+                </tr>
+              );
+            })}
         </tbody>
       </table>
     </div>
